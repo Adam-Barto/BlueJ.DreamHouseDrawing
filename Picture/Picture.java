fcb9cea6b432fe2e2dce44b829fdabcebd90bca5
Adam B <adamb@Zipcoders-MacBook-Pro.local>
2023-02-28 10:27:23 -0500

    Made a house

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -13,6 +13,13 @@
 public class Picture
 {
     private Square wall;
+    private Square s;
+    private Square s1;
+    private Square t;
+    private Triangle t1;
+    private Square wall2;
+    private Square wall3;
+    private Square wall4;
     private Square window;
     private Triangle roof;
     private Circle sun;
@@ -30,28 +37,105 @@ public class Picture
      */
     public void draw()
     {
+        s = new Square();
+        s.changeColor("blue");
+        s.moveVertical(-100);
+        s.moveHorizontal(-100);
+        s.changeSize(500);
+        s.makeVisible();
+        
+        s = new Square();
+        s.changeColor("yellow");
+        s.moveVertical(140);
+        s.moveHorizontal(-70);
+        s.changeSize(500);
+        s.makeVisible();
+        
+        t = new Square();
+        t.changeColor("black");
+        t.moveHorizontal(-47);
+        t.moveVertical(137);
+        t.changeSize(50);
+        t.makeVisible();
+        
+        t1 = new Triangle();
+        t1.changeColor("yellow");
+        t1.moveVertical(172);
+        t1.moveHorizontal(-40);
+        t1.changeSize(50,100);
+        t1.makeVisible();
+        
+        s1 = new Square();
+        s1.changeColor("blue");
+        s1.moveVertical(40);
+        s1.moveHorizontal(-70);
+        s1.changeSize(100);
+        s1.makeVisible();
+        
         wall = new Square();
-        wall.moveVertical(80);
+        wall.changeColor("black");
+        wall.moveVertical(90);
         wall.changeSize(100);
         wall.makeVisible();
+        
+        wall2 = new Square();
+        wall2.changeColor("white");
+        wall2.moveVertical(92);
+        wall2.moveHorizontal(2);
+        wall2.changeSize(96);
+        wall2.makeVisible();
+        
+        wall3 = new Square();
+        wall3.changeColor("black");
+        //wall3.moveVertical(90);
+        wall3.changeSize(100);
+        wall3.makeVisible();
+        
+        wall4 = new Square();
+        wall4.changeColor("white");
+        wall4.moveVertical(2);
+        wall4.moveHorizontal(2);
+        wall4.changeSize(96);
+        wall4.makeVisible();
+        
+        wall4 = new Square();
+        wall4.changeColor("white");
+        wall4.moveVertical(10);
+        wall4.moveHorizontal(2);
+        wall4.changeSize(96);
+        wall4.makeVisible();
 
         window = new Square();
         window.changeColor("black");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
+        window.moveHorizontal(35);
+        window.moveVertical(140);
+        window.makeVisible();
+        
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(35);
+        window.moveVertical(160);
         window.makeVisible();
 
         roof = new Triangle();
-        roof.changeSize(50, 140);
+        roof.changeColor("black");
+        roof.changeSize(30, 30);
         roof.moveHorizontal(60);
         roof.moveVertical(70);
         roof.makeVisible();
+        
+        roof = new Triangle();
+        roof.changeColor("black");
+        roof.changeSize(-30, 30);
+        roof.moveHorizontal(60);
+        roof.moveVertical(130);
+        roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("yellow");
+        sun.changeColor("red");
         sun.moveHorizontal(180);
         sun.moveVertical(-10);
-        sun.changeSize(60);
+        sun.changeSize(100);
         sun.makeVisible();
     }
 
